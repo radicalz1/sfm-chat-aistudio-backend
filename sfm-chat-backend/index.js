ea2cb29f022cc1e0d7ac580541ecd900c8ebf289
radicalz1 <142083579+radicalz1@users.noreply.github.com>
2025-02-21 20:53:12 +0000

    fix replit and bak env

--- sfm-chat-backend/index.js
+++ sfm-chat-backend/index.js
@@ -13,7 +13,7 @@ app.use(bodyParser.json()); // Parse JSON request bodies
 
 // MongoDB Connection
 const uri = process.env.MONGODB_URI; // Get MongoDB URI from .env file
-mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect(uri, { });
 const connection = mongoose.connection;
 connection.once('open', () => {
   console.log("MongoDB database connection established successfully");
@@ -22,6 +22,15 @@ connection.once('open', () => {
 // Simple route for testing
 app.get('/', (req, res) => {
   res.send('Backend server is running!');
+});
+
+// Endpoint API untuk menerima pesan dari aplikasi pasien
+app.post('/messages', (req, res) => {
+  const message = req.body; // Data pesan dari body request
+  console.log('Pesan diterima dari pasien:', message); // Log pesan yang diterima di server
+
+  // Kirim respons sukses ke aplikasi pasien
+  res.status(200).json({ message: 'Pesan diterima server!' });
 });
 
 app.listen(port, () => {
